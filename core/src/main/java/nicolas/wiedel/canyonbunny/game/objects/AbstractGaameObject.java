90c67e15b84114e3955aeede226e2cbf202036f0
nwiedel <164350832+nwiedel@users.noreply.github.com>
2025-05-16 08:45:11 +0200

    Kapitel 5: Zeichen der Felsen
    Untergrund für unseren Player

---
+++ core/src/main/java/nicolas/wiedel/canyonbunny/game/objects/AbstractGaameObject.java
@@ -0,0 +1,30 @@
+package nicolas.wiedel.canyonbunny.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Grundlegende Klasse für alle Game Objekte
+ */
+public abstract class AbstractGaameObject {
+
+    public Vector2 position;
+    public Vector2 dimension;
+    public Vector2 origin;
+    public Vector2 scale;
+    public float rotation;
+
+    public AbstractGaameObject(){
+        position = new Vector2();
+        dimension = new Vector2(1, 1);
+        origin = new Vector2();
+        scale = new Vector2(1, 1);
+        rotation = 0;
+    }
+
+    public void update(float deltaTime){
+
+    }
+
+    public abstract void render(SpriteBatch batch);
+}
---
+++ core/src/main/java/nicolas/wiedel/canyonbunny/game/objects/Rock.java
@@ -0,0 +1,86 @@
+package nicolas.wiedel.canyonbunny.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import nicolas.wiedel.canyonbunny.game.Assets;
+
+/**
+ * Klasse für die Felsen, die als Untergrud dienen
+ */
+public class Rock extends AbstractGaameObject {
+
+    public TextureRegion regEdge;
+    public TextureRegion regMiddle;
+
+    private int length;
+
+    public Rock(){
+        init();
+    }
+
+    private void init(){
+        dimension.set(1f, 1.5f);
+
+        regEdge = Assets.INSTANCE.rock.edge;
+        regMiddle = Assets.INSTANCE.rock.middle;
+
+        // Grundlegend ist die länge des Rocks 1
+        setLength(1);
+    }
+
+    public void setLength(int lenght){
+        this.length = lenght;
+    }
+
+    public void increaseLength(int amount){
+        setLength(length + amount);
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        TextureRegion reg = null;
+
+        float relX = 0;
+        float relY = 0;
+
+        // linke Seite zeichnen
+        reg = regEdge;
+        relX -= dimension.x / 4;
+        batch.draw(reg.getTexture(),
+            position.x + relX, position.y + relY,
+            origin.x, origin.y,
+            dimension.x / 4, dimension.y,
+            scale.x, scale.y,
+            rotation,
+            reg.getRegionX(), reg.getRegionY(),
+            reg.getRegionWidth(), reg.getRegionHeight(),
+            false, false);
+
+        // Mitte zeichnen
+        reg = regMiddle;
+        relX = 0;
+        for (int i = 0; i < length; i++){
+            batch.draw(reg.getTexture(),
+                position.x + relX, position.y + relY,
+                origin.x, origin.y,
+                dimension.x , dimension.y,
+                scale.x, scale.y,
+                rotation,
+                reg.getRegionX(), reg.getRegionY(),
+                reg.getRegionWidth(), reg.getRegionHeight(),
+                false, false);
+        }
+
+        // rechte Seite zeichnen
+        reg = regEdge;
+        batch.draw(reg.getTexture(),
+            position.x + relX, position.y + relY,
+            origin.x + dimension.x / 8, origin.y,
+            dimension.x / 4, dimension.y,
+            scale.x, scale.y,
+            rotation,
+            reg.getRegionX(), reg.getRegionY(),
+            reg.getRegionWidth(), reg.getRegionHeight(),
+            true, false);
+    }
+}
